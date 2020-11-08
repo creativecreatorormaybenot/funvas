0af37349b658154b9e9893e885062bbda5814959
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-08 08:03:20 +0000

    Examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,18 +13,47 @@ class ExampleApp extends StatelessWidget {
   /// Constructs [ExampleApp].
   const ExampleApp({Key key}) : super(key: key);
 
+  /// List of example funvas implementations.
+  static final examples = <Funvas>[
+    ExampleFunvas(),
+    WaveFunvas(),
+    OrbsFunvas(),
+  ];
+
+  static int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: FunvasContainer(
-              funvas: ExampleFunvas(),
-            ),
-          ),
-        ),
+        body: StatefulBuilder(builder: (context, setState) {
+          return Stack(
+            children: [
+              Center(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: FunvasContainer(
+                    funvas: examples[_selectedIndex],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(32),
+                  child: OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = (_selectedIndex + 1) % examples.length;
+                      });
+                    },
+                    child: const Text('Next'),
+                  ),
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
@@ -41,5 +70,45 @@ class ExampleFunvas extends Funvas {
       S(t).abs() * x.height / 4 + 42,
       Paint()..color = R(C(t) * 255, 42, 60 + T(t)),
     );
+  }
+}
+
+/// Funvas adapted 1:1 from https://www.dwitter.net/d/3713.
+class WaveFunvas extends Funvas {
+  @override
+  void u(double t) {
+    c.scale(x.width / 1920, x.height / 1080);
+
+    for (int i = 0; i < 64; i++) {
+      c.drawRect(
+        Rect.fromLTWH(
+          i * 30.0,
+          400 + C(4 * t + (i * 3)) * 100,
+          27,
+          200,
+        ),
+        Paint(),
+      );
+    }
+  }
+}
+
+/// Funvas adapted 1:1 from https://www.dwitter.net/d/4342.
+class OrbsFunvas extends Funvas {
+  @override
+  void u(double t) {
+    c.scale(x.width / 1920, x.height / 1080);
+
+    final v = t + 400;
+    for (int q = 255; q > 0; q--) {
+      final paint = Paint()..color = R(q, q, q);
+      c.drawCircle(
+          Offset(
+            1920 / 2 + C(v - q) * (v + q),
+            540 + S(v - q) * (v - q),
+          ),
+          40,
+          paint);
+    }
   }
 }
