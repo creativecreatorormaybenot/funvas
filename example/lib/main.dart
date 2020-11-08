801911e75a8ebd9c5fdbdb1ea8f6bc796d5c26b2
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-08 10:25:22 +0000

    Version 1 (#2)
    * Examples
    
    * Setup tweets section
    
    * Add first animation
    
    * Add tweet
    
    * Version one
    
    * Remove example
    
    * Format
    
    * Remove unused
    
    * Fix import
    
    * Fix analysis options
    
    * Omit annotations

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:example/tweets/tweets.dart';
+import 'package:example/viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:funvas/funvas.dart';
 
@@ -13,16 +15,34 @@ class ExampleApp extends StatelessWidget {
   /// Constructs [ExampleApp].
   const ExampleApp({Key key}) : super(key: key);
 
+  /// List of example funvas implementations.
+  static final examples = <Funvas>[
+    ExampleFunvas(),
+    WaveFunvas(),
+    OrbsFunvas(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: FunvasContainer(
-              funvas: ExampleFunvas(),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('creativecreatorormaybenot Funvas'),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: [
+                Tab(text: 'Examples'),
+                Tab(text: '@creativemaybeno tweets'),
+              ],
             ),
+          ),
+          body: TabBarView(
+            children: [
+              FunvasViewer(funvases: ExampleApp.examples),
+              FunvasViewer(funvases: creativecreatorormaybenotTweets),
+            ],
           ),
         ),
       ),
@@ -41,5 +61,45 @@ class ExampleFunvas extends Funvas {
       S(t).abs() * x.height / 4 + 42,
       Paint()..color = R(C(t) * 255, 42, 60 + T(t)),
     );
+  }
+}
+
+/// Funvas adapted 1:1 from https://www.dwitter.net/d/3713.
+class WaveFunvas extends Funvas {
+  @override
+  void u(double t) {
+    c.scale(x.width / 1920, x.height / 1080);
+
+    for (var i = 0; i < 64; i++) {
+      c.drawRect(
+        Rect.fromLTWH(
+          i * 30.0,
+          400 + C(4 * t + (i * 3)) * 100,
+          27,
+          200,
+        ),
+        Paint(),
+      );
+    }
+  }
+}
+
+/// Funvas adapted 1:1 from https://www.dwitter.net/d/4342.
+class OrbsFunvas extends Funvas {
+  @override
+  void u(double t) {
+    c.scale(x.width / 1920, x.height / 1080);
+
+    final v = t + 400;
+    for (var q = 255; q > 0; q--) {
+      final paint = Paint()..color = R(q, q, q);
+      c.drawCircle(
+          Offset(
+            1920 / 2 + C(v - q) * (v + q),
+            540 + S(v - q) * (v - q),
+          ),
+          40,
+          paint);
+    }
   }
 }
---
+++ example/lib/tweets/1.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:funvas/funvas.dart';
+
+/// todo(creativecreatorormaybe): add tweet link.
+class One extends Funvas {
+  @override
+  void u(double t) {
+    final w = x.width,
+        h = x.height,
+        m = min(w, h),
+        center = Offset(w / 2, h / 2);
+
+    // Draw background.
+    c.drawPaint(Paint()..color = R(242, 227, 193));
+
+    final outer = m * (.42 + C(t / 3) * 5e-2);
+
+    // Draw background circle.
+    c.drawCircle(
+      center,
+      outer,
+      Paint()..color = R(50, 71, 104),
+    );
+
+    const padding = 8;
+
+    // Draws a small circle.
+    void sc(double delta, double radius) {
+      void draw(double minorDelta) {
+        c.drawCircle(
+          center +
+              Offset.fromDirection(
+                  T(S(t + pow(.2, delta))) * pi * 2 + minorDelta / 100,
+                  outer - padding - radius - minorDelta / 10),
+          radius - minorDelta / 10,
+          Paint()
+            ..color = R(200 + delta * 40, 100 + delta * 80, 100 - minorDelta,
+                .1 + delta / 5),
+        );
+      }
+
+      for (var i = 0; i < delta * 10; i++) {
+        draw(i * 7.0);
+      }
+    }
+
+    for (var i = 0; i < 16; i++) {
+      sc(i / 2 / 10, 120.0 - i * 7);
+    }
+  }
+}
