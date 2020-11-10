276ed51dcef08e7e37eaf368966756d803b24e45
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-10 21:23:08 +0000

    Create two (#5)
    * Create two
    
    * Update example/lib/tweets/tweets.dart

--- example/lib/tweets/tweets.dart
+++ example/lib/tweets/tweets.dart
@@ -1,9 +1,11 @@
 import 'package:example/tweets/1.dart';
+import 'package:example/tweets/2.dart';
 import 'package:funvas/funvas.dart';
 
 /// This file contains funvases from creativecreatorormaybenot's tweets.
 /// You can find them in the timeline here: https://twitter.com/creativemaybeno.
 
 final creativecreatorormaybenotTweets = <Funvas>[
+  Two(),
   One(),
 ];
