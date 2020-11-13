e31ddbb203ffbfb2f6b3997d3feb3cd9d18322ef
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-13 17:22:13 +0000

    Add exporting capabilities

--- example/README.md
+++ example/README.md
@@ -1,3 +1,32 @@
 # example
 
 Example app showcasing how to use the funvas package.
+Additionally, this project contains code for exporting funvas animations to GIFs.
+
+## Exporting animations
+
+You will need [gifsicle] for this. The installation is as easy as running `brew install gifsicle`.
+If you are not on macOS, you will need to read through installation guides yourself.
+Additionally, you need to have [ImageMagick] to convert PNGs to individual GIFs as gifsicle only
+supports GIF input. This can also be as easy as using `brew install imagemagick`.
+
+The script for exporting animations is a **bash script**. If you do not have access to Bash, you
+will have to execute the commands manually.
+
+### Usage
+
+You can simply edit the `export/export_animation.dart` file to adjust the settings of the exporter.
+
+You can configure the following parameters:
+* FPS
+* Animation duration
+* Dimensions
+* The funvas to be used :)
+
+The exporter uses `flutter_test` and its **goldens files** to generate the frames. Those will be
+saved at `export/<animation name>/<frame number>.gif`. gifsicle will then assemble a GIF from the
+frames. Note that you will also need to adjust the bash script (`export/export_gif.sh`) to use your
+frames (the path) and also your frame rate (the delay is given in milliseconds, i.e. the delay
+between two frames - you will have to calculate that yourself).
+
+[gifsicle]: http://www.lcdf.org/gifsicle
---
+++ example/export/export_animation.dart
@@ -0,0 +1,57 @@
+import 'package:example/tweets/3.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/src/_matchers_io.dart';
+import 'package:funvas/src/painter.dart';
+
+void main() {
+  const fps = 50;
+  const animationDuration = Duration(seconds: 10);
+  const dimensions = Size(750, 750);
+  // If you use a different animation name, you will have to also consider that
+  // when exporting to GIF.
+  const animationName = 'animation';
+  final funvas = Three();
+
+  ValueNotifier<double> time;
+
+  setUpAll(() {
+    time = ValueNotifier<double>(0);
+  });
+
+  tearDownAll(() {
+    time.dispose();
+  });
+
+  testWidgets('export funvas animation', (tester) async {
+    await tester.binding.setSurfaceSize(dimensions);
+    tester.binding.window.physicalSizeTestValue = dimensions;
+    tester.binding.window.devicePixelRatioTestValue = 1;
+
+    await tester.pumpWidget(SizedBox.fromSize(
+      size: dimensions,
+      child: CustomPaint(
+        painter: FunvasPainter(
+          time: time,
+          delegate: funvas,
+        ),
+      ),
+    ));
+
+    final microseconds = animationDuration.inMicroseconds,
+        goldensNeeded = fps * (microseconds / 1e6) ~/ 1;
+
+    final fileNameWidth = (goldensNeeded - 1).toString().length;
+
+    for (var i = 0; i < goldensNeeded; i++) {
+      time.value = 1 / fps * i;
+      await tester.pump();
+
+      final matcher = MatchesGoldenFile.forStringPath(
+          '$animationName/${'$i'.padLeft(fileNameWidth, '0')}'
+          '.png',
+          null);
+      await matcher.matchAsync(find.byType(SizedBox));
+    }
+  });
+}
