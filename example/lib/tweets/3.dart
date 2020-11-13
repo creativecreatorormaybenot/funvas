c8c46555ef7341f196670fceeb58e91c9deaafaa
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-13 01:33:22 +0000

    Finish animation

--- example/lib/tweets/3.dart
+++ example/lib/tweets/3.dart
@@ -1,29 +1,43 @@
 import 'dart:math';
 import 'dart:ui';
 
+import 'package:flutter/animation.dart';
 import 'package:funvas/funvas.dart';
 
 /// todo(creativecreatorormaybenot): add tweet link.
 class Three extends Funvas {
   @override
   void u(double t) {
-    final backgroundPaint = Paint()..color = Color(0xff0099cc),
-        foregroundPaint = Paint()..color = Color(0xffcc0000);
+    final backgroundPaint = Paint()..color = Color(0xff600060),
+        foregroundPaint = Paint()..color = Color(0xeeadffd2);
 
     c.drawPaint(backgroundPaint);
 
     final rect = Offset.zero & Size(x.width, x.height);
 
     void drawBall(double radians, double distance, double radius) {
-      final p = rect.center + Offset.fromDirection(radians, distance);
+      final p = rect.center + Offset.fromDirection(-radians - pi / 2, distance);
 
-      c.drawLine(rect.center, p, foregroundPaint..strokeWidth = 2);
+      c.drawLine(rect.center, p, foregroundPaint..strokeWidth = 1);
       c.drawCircle(p, radius, foregroundPaint);
     }
 
-    drawBall(0, 42, 14);
+    c.translate(x.width / 2, x.height / 2);
+    c.rotate(C(t));
+    c.translate(-x.width / 2, -x.height / 2);
+
+    final count = 28;
+    for (var i = 0; i < count; i++) {
+      final addend = (i / count) * .5,
+          curve = Cubic(.4 + addend, 0, .4 + addend, 1);
+      drawBall(
+        curve.transform((t - i / 12) / 2.5 % 1) * 2 * pi,
+        40.0 + i * ((x.width / 2 - 56) / count),
+        9 + i / 6,
+      );
+    }
 
     // Draw center dot.
-    c.drawCircle(rect.center, 6, foregroundPaint);
+    c.drawCircle(rect.center, 5, foregroundPaint);
   }
 }
