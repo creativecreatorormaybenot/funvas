4f869ba06d88e0e51f111f191ec555bdb49794fe
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-16 00:42:28 +0000

    Complete animation

--- example/lib/tweets/4.dart
+++ example/lib/tweets/4.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:funvas/funvas.dart';
 
@@ -19,31 +20,25 @@ class Four extends Funvas {
 
     c.drawPaint(backgroundPaint);
 
-    // The distance is (obviously) used both for spacing and movement distance
-    // because we want to create a perfect loop, which requires the movement
-    // of one arrow to end up at the location of another arrow. Hence, the
-    // spacing needs to equal the movement distance.
-    const distance = 42.0;
+    const distance = 80.0;
 
     final arrowPainter = TextPainter(
-      text: const TextSpan(
-        text: '→',
-        style: TextStyle(
-          fontSize: 20,
+      text: TextSpan(
+        text: String.fromCharCode(0xe5b3),
+        style: const TextStyle(
+          fontFamily: 'MaterialIcons',
+          fontSize: 56,
         ),
       ),
       textDirection: TextDirection.ltr,
     );
     arrowPainter.layout();
 
-    void paintArrow(
-        Offset center,
-        // The rotation progress between 0 and 1.
-        double rotation) {
+    void paintArrow(Offset center, double rotation) {
       c.save();
-      c.translate(x.width / 2, x.height / 2);
-      c.rotate(rotation * pi * 2);
-      c.translate(-x.width / 2, -x.height / 2);
+      c.translate(center.dx, center.dy);
+      c.rotate(rotation);
+      c.translate(-center.dx, -center.dy);
 
       final rect = Rect.fromCenter(
         center: center,
@@ -55,11 +50,248 @@ class Four extends Funvas {
       c.restore();
     }
 
-    paintArrow((Offset.zero & Size(x.width, x.height)).center,
-        rotationCurve.transform(t % 1));
-    paintArrow(
-        (Offset.zero & Size(x.width, x.height)).center +
-            Offset.fromDirection(pi / 4, distance),
-        rotationCurve.transform((t + .1) % 1));
+    // Describes the offset that should be moved to every second.
+    final movementPath = TweenSequence<Offset>([
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset.zero,
+          end: Offset.zero,
+        ),
+        weight: 1,
+      ),
+      // Move right.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset.zero,
+          end: Offset(distance, 0),
+        ).chain(CurveTween(curve: movementCurve)),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance, 0),
+          end: Offset(distance, 0),
+        ),
+        weight: 1 / 2,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance, 0),
+          end: Offset(distance, 0),
+        ),
+        weight: 1,
+      ),
+      // Move top right.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance, 0),
+          end: Offset(distance + distance / 2, -distance),
+        ).chain(CurveTween(curve: movementCurve)),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance + distance / 2, -distance),
+          end: Offset(distance + distance / 2, -distance),
+        ),
+        weight: 1 / 2,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance + distance / 2, -distance),
+          end: Offset(distance + distance / 2, -distance),
+        ),
+        weight: 1,
+      ),
+      // Move left.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance + distance / 2, -distance),
+          end: Offset(distance / 2, -distance),
+        ).chain(CurveTween(curve: movementCurve)),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance / 2, -distance),
+          end: Offset(distance / 2, -distance),
+        ),
+        weight: 1 / 2,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance / 2, -distance),
+          end: Offset(distance / 2, -distance),
+        ),
+        weight: 1,
+      ),
+      // Move bottom left.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(distance / 2, -distance),
+          end: Offset(0, 0),
+        ).chain(CurveTween(curve: movementCurve)),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: Offset(0, 0),
+          end: Offset(0, 0),
+        ),
+        weight: 1 / 2,
+      ),
+    ]);
+
+    // The wait sections are shared. The stay put sections are reversed
+    // compared to the movement.
+    final rotations = TweenSequence<double>([
+      // Rotate to prepare to move right.
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: -1.25 * pi,
+          end: 0,
+        ).chain(CurveTween(curve: rotationCurve)),
+        weight: 1,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 0,
+          end: 0,
+        ),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 0,
+          end: 0,
+        ),
+        weight: 1 / 2,
+      ),
+      // Rotate to prepare moving top right.
+      TweenSequenceItem(
+        tween: Tween<double>(
+          begin: 0,
+          end: 1.75 * pi,
+        ).chain(CurveTween(curve: rotationCurve)),
+        weight: 1,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.75 * pi,
+          end: 1.75 * pi,
+        ),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.75 * pi,
+          end: 1.75 * pi,
+        ),
+        weight: 1 / 2,
+      ),
+      // Rotate to prepare moving left.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.75 * pi,
+          end: 3 * pi,
+        ).chain(CurveTween(curve: rotationCurve)),
+        weight: 1,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 3 * pi,
+          end: 3 * pi,
+        ),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 3 * pi,
+          end: 3 * pi,
+        ),
+        weight: 1 / 2,
+      ),
+      // Rotate to prepare moving bottom left.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 3 * pi,
+          end: 4.75 * pi,
+        ).chain(CurveTween(curve: rotationCurve)),
+        weight: 1,
+      ),
+      // Stay put.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 4.75 * pi,
+          end: 4.75 * pi,
+        ),
+        weight: 1,
+      ),
+      // Wait.
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 4.75 * pi,
+          end: 4.75 * pi,
+        ),
+        weight: 1 / 2,
+      ),
+    ]);
+
+    const animationSeconds = 10;
+
+    if (t >= animationSeconds) {
+      t = t % animationSeconds;
+    }
+
+    // Assuming that we wait for 0.5 seconds after every 2 seconds, we can be
+    // sure that nothing is rotating or whatever during that time, which allows
+    // us to translate the canvas in order to cheat with the movement :)
+    final translationOffset = t < 2.5
+        ? Offset.zero
+        : movementPath.transform(((t ~/ 2.5) * 2.5) / animationSeconds);
+    c.translate(-translationOffset.dx, -translationOffset.dy);
+
+    // Delays the movements the further the arrows are away from the center.
+    double outwardsDelayedT(int w, int h) {
+      return t - (w.abs() + h.abs()) / 25;
+    }
+
+    Offset movementOffset(int w, int h) {
+      final t = max(0, outwardsDelayedT(w, h));
+      return movementPath.transform(t / animationSeconds);
+    }
+
+    double rotation(int w, int h) {
+      final t = max(0, outwardsDelayedT(w, h));
+      return rotations.transform(t / animationSeconds);
+    }
+
+    for (var h = 0; h < 15; h++) {
+      for (var w = 0; w < 15; w++) {
+        final base = (Offset.zero & Size(x.width, x.height)).center +
+            Offset(h.isOdd ? distance / 2 : 0, 0) +
+            movementOffset(w, h);
+        final radians = rotation(w, h);
+
+        paintArrow(base + Offset(w * distance, h * distance), radians);
+        paintArrow(base + Offset(w * distance, -h * distance), radians);
+        paintArrow(base + Offset(-w * distance, h * distance), radians);
+        paintArrow(base + Offset(-w * distance, -h * distance), radians);
+      }
+    }
   }
 }
