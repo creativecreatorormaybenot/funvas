5c7b711527c92f067f19f6b3bfbb060028fdce06
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-16 02:16:28 +0000

    8 seconds

--- example/export/export_animation.dart
+++ example/export/export_animation.dart
@@ -9,7 +9,7 @@ import 'package:funvas/src/painter.dart';
 
 void main() async {
   const fps = 50;
-  const animationDuration = Duration(seconds: 10);
+  const animationDuration = Duration(seconds: 8);
   const dimensions = Size(750, 750);
   // If you use a different animation name, you will have to also consider that
   // when exporting to GIF.
