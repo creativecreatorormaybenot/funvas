288ba48cee0036b1523bfefd592d3c35cb09e3d6
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-16 02:13:10 +0000

    Final polish

--- example/lib/tweets/4.dart
+++ example/lib/tweets/4.dart
@@ -10,11 +10,13 @@ import 'package:funvas/funvas.dart';
 class Four extends Funvas {
   // The movement curve should be harsher than the rotation curve because
   // the rotation should prepare for the movement.
-  static const _movementCurve = Cubic(.68, .03, .31, .98),
+  static const _movementCurve = Cubic(.47, .04, .31, .98),
       _rotationCurve = Cubic(.51, .15, .31, .88);
 
   /// Vertical and horizontal spacing.
   static const _distance = 83.3;
+
+  static const _animationSeconds = 8;
 
   /// Describes the offset that should be moved to every second.
   final _movementPath = TweenSequence<Offset>([
@@ -122,7 +124,7 @@ class Four extends Funvas {
     // Rotate to prepare to move right.
     TweenSequenceItem(
       tween: Tween<double>(
-        begin: -1.25 * pi,
+        begin: -1.325 * pi,
         end: 0,
       ).chain(CurveTween(curve: _rotationCurve)),
       weight: 1,
@@ -147,30 +149,30 @@ class Four extends Funvas {
     TweenSequenceItem(
       tween: Tween<double>(
         begin: 0,
-        end: 1.75 * pi,
+        end: 1.65 * pi,
       ).chain(CurveTween(curve: _rotationCurve)),
       weight: 1,
     ),
     // Stay put.
     TweenSequenceItem(
       tween: Tween(
-        begin: 1.75 * pi,
-        end: 1.75 * pi,
+        begin: 1.675 * pi,
+        end: 1.675 * pi,
       ),
       weight: 1,
     ),
     // Wait.
     TweenSequenceItem(
       tween: Tween(
-        begin: 1.75 * pi,
-        end: 1.75 * pi,
+        begin: 1.675 * pi,
+        end: 1.675 * pi,
       ),
       weight: 1 / 2,
     ),
     // Rotate to prepare moving left.
     TweenSequenceItem(
       tween: Tween(
-        begin: 1.75 * pi,
+        begin: 1.675 * pi,
         end: 3 * pi,
       ).chain(CurveTween(curve: _rotationCurve)),
       weight: 1,
@@ -195,23 +197,23 @@ class Four extends Funvas {
     TweenSequenceItem(
       tween: Tween(
         begin: 3 * pi,
-        end: 4.75 * pi,
+        end: 4.675 * pi,
       ).chain(CurveTween(curve: _rotationCurve)),
       weight: 1,
     ),
     // Stay put.
     TweenSequenceItem(
       tween: Tween(
-        begin: 4.75 * pi,
-        end: 4.75 * pi,
+        begin: 4.675 * pi,
+        end: 4.675 * pi,
       ),
       weight: 1,
     ),
     // Wait.
     TweenSequenceItem(
       tween: Tween(
-        begin: 4.75 * pi,
-        end: 4.75 * pi,
+        begin: 4.675 * pi,
+        end: 4.675 * pi,
       ),
       weight: 1 / 2,
     ),
@@ -251,21 +253,19 @@ class Four extends Funvas {
       c.restore();
     }
 
-    const animationSeconds = pi * 2.75;
-
-    if (t >= animationSeconds) {
-      t = t % animationSeconds;
+    if (t >= _animationSeconds) {
+      t = t % _animationSeconds;
     }
 
     // Assuming that we wait for weight 1 / 2 seconds after every weight 2
     // seconds, we can be sure that nothing is rotating or whatever during that
     // time, which allows us to translate the canvas in order to cheat with the
     // movement :)
-    final translationOffset = t < animationSeconds / 4
+    final translationOffset = t < _animationSeconds / 4
         ? Offset.zero
         : _movementPath.transform(
-            ((t ~/ (animationSeconds / 4)) * animationSeconds / 4) /
-                animationSeconds);
+            ((t ~/ (_animationSeconds / 4)) * _animationSeconds / 4) /
+                _animationSeconds);
     c.translate(-translationOffset.dx, -translationOffset.dy);
 
     final center = Offset(x.width / 2, x.height / 2);
@@ -273,17 +273,17 @@ class Four extends Funvas {
     // Delays the movements the further the arrows are away from the center.
     double outwardsDelayedT(Offset position) {
       if (center == position) return t;
-      return t - (center - position).distance / 1.2e3;
+      return t - (center - position).distance / 1.3e3;
     }
 
     Offset movementOffset(Offset position) {
       final t = max(0, outwardsDelayedT(position));
-      return _movementPath.transform(t / animationSeconds);
+      return _movementPath.transform(t / _animationSeconds);
     }
 
     double rotation(Offset position) {
       final t = max(0, outwardsDelayedT(position));
-      return _rotations.transform(t / animationSeconds);
+      return _rotations.transform(t / _animationSeconds);
     }
 
     for (var h = 0; h < 15; h++) {
