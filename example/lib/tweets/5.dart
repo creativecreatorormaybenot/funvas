0ace9980bdae5b521f48b00f7818971e0d5efbd0
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-17 14:51:46 +0000

    Add vinca example (#13)
    * Add example
    
    * More credit
    
    * Update action

---
+++ example/lib/tweets/5.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:funvas/funvas.dart';
+
+/// todo: add link.
+///
+/// You can trace the full origin of this code using the link above, however,
+/// I want to point out more clearly that the algorithm for this came from
+/// vinca initially and not from me. You an follow the remix origin of my dweet
+/// at https://www.dwitter.net/d/14681 in order to see the JavaScript origin
+/// of the code, even before I migrated it to Dart initially.
+class Five extends Funvas {
+  @override
+  void u(double t) {
+    c.drawPaint(Paint()..color = Color(0xffffffff));
+
+    for (var A = .0, q = 123, j = .0, i = 756;
+        i-- > 0;
+        c.drawRect(
+      Rect.fromLTWH(
+          x.width / 2 + A * sin(j), x.height / 2 + A * cos(j), i / 84, i / 84),
+      Paint()..color = Color.fromRGBO(i % 99 + 156, q - i % q, q, 1),
+    )) {
+      j = i / 9;
+      A = (9 * sin(t * j / 20) + cos(20 * j) + 6) * 21;
+    }
+  }
+}
