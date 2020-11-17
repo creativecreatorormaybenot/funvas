deba80aade9cc74d73a15beabd18508ad8ae031b
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-17 14:39:40 +0000

    More credit

--- example/lib/tweets/5.dart
+++ example/lib/tweets/5.dart
@@ -4,6 +4,12 @@ import 'dart:ui';
 import 'package:funvas/funvas.dart';
 
 /// todo: add link.
+///
+/// You can trace the full origin of this code using the link above, however,
+/// I want to point out more clearly that the algorithm for this came from
+/// vinca initially and not from me. You an follow the remix origin of my dweet
+/// at https://www.dwitter.net/d/14681 in order to see the JavaScript origin
+/// of the code, even before I migrated it to Dart initially.
 class Five extends Funvas {
   @override
   void u(double t) {
