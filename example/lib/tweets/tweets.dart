cf7be2df07e64e87104fe2d9b5d9d4b2d7f65a22
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-30 12:54:44 +0000

    Draw Julia sets (#22)

--- example/lib/tweets/tweets.dart
+++ example/lib/tweets/tweets.dart
@@ -5,12 +5,14 @@ import 'package:example/tweets/4.dart';
 import 'package:example/tweets/5.dart';
 import 'package:example/tweets/6.dart';
 import 'package:example/tweets/7.dart';
+import 'package:example/tweets/8.dart';
 import 'package:funvas/funvas.dart';
 
 /// This file contains funvases from creativecreatorormaybenot's tweets.
 /// You can find them in the timeline here: https://twitter.com/creativemaybeno.
 
 final creativecreatorormaybenotTweets = <Funvas>[
+  Eight(),
   Seven(),
   Six(),
   Five(),
