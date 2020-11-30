5411a79a28e58b9ba2e299484c37c2ea93bd1e55
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-11-30 21:16:46 +0000

    Use sound null safety for exporting & opt-out stable CI (#23)
    * Use sound null safety for exporting
    
    * Stable matrix

--- example/export/flutter_test_config.dart
+++ example/export/flutter_test_config.dart
@@ -1,9 +1,5 @@
 import 'dart:async';
 
-// Ignoring because this is only a dev_dependency and the actual example does
-// run with sound null safety.
-// Issue for golden_toolkit null safety can be found here: https://github.com/eBay/flutter_glove_box/issues/90
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
