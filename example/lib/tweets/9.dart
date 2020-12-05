e0bd246fe94857c5711dcfd7aa4b4f105d8a7ec9
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-12-05 14:25:07 +0000

    Add Julia sets variation

--- example/lib/tweets/9.dart
+++ example/lib/tweets/9.dart
@@ -4,8 +4,8 @@ import 'dart:ui';
 import 'package:flutter/painting.dart';
 import 'package:funvas/funvas.dart';
 
-/// https://twitter.com/creativemaybeno/status/1333757489670746112?s=20
-class Eight extends Funvas {
+/// todo: add link.
+class Nine extends Funvas {
   // The maximum number of iterations that we test for the threshold for a
   // given complex number.
   static const _iterations = 100;
@@ -15,28 +15,27 @@ class Eight extends Funvas {
     // The center point in the Mandelbrot set about which we rotate to draw
     // corresponding Julia sets.
     const centerPoint = [
-      -0.38551857,
-      -0.18918556,
+      -0.75,
+      0,
     ];
-    // The radius which we rotate about the center point in the Mandelbrot set.
-    const rotationRadius = 0.35;
     // The duration (in seconds) for completing the rotation about the center
     // point in the Mandelbrot set once.
-    const rotationDuration = 5;
+    const rotationDuration = 9;
 
     // Value that the sum of the real and imaginary component needs to surpass
     // for the complex number to be considered **not** part of the set.
     // This is arbitrary, so yeah.
     const threshold = 16;
     // The coordinate range I feel you want to see of the set at 0 zoom.
-    final reRange = <double>[-1.65, 1.65], imRange = <double>[-1.65, 1.65];
+    final zoom = cos(t % rotationDuration / rotationDuration * 2 * pi) * .5;
+    final reRange = <double>[-1.65 + .65 - zoom, 1.65 - .65 + zoom],
+        imRange = <double>[-1.65 + .65 - zoom, 1.65 - .65 + zoom];
 
     final c = [
       centerPoint[0] +
-          sin(t % rotationDuration / rotationDuration * 2 * pi) *
-              rotationRadius,
+          cos(t % rotationDuration / rotationDuration * 2 * pi) * .05,
       centerPoint[1] +
-          cos(t % rotationDuration / rotationDuration * 2 * pi) * rotationRadius
+          sin(t % rotationDuration / rotationDuration * 2 * pi) * .1,
     ];
 
     for (var x = 0; x < this.x.width; x++) {
@@ -56,13 +55,13 @@ class Eight extends Funvas {
         }
 
         this.c.drawRect(
-              Rect.fromLTWH(x / 1, y / 1, 1, 1),
-              Paint()
-                ..color = n == _iterations
-                    ? const Color(0xff000000)
-                    : HSVColor.fromAHSV(1, 360 * n / _iterations, .9, .6)
-                        .toColor(),
-            );
+          Rect.fromLTWH(x / 1, y / 1, 1, 1),
+          Paint()
+            ..color = n == _iterations
+                ? const Color(0xff000000)
+                : HSVColor.fromAHSV(1, 360 * n / _iterations, .9, .6)
+                .toColor(),
+        );
       }
     }
   }
