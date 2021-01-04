3d1f45e02bf4c04088122f29edc29abe23e0e12f
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-01-04 02:38:14 +0000

    Add eleven (#44)
    * Add eleven
    
    * Increase sizes

--- funvas_tweets/lib/funvas_tweets.dart
+++ funvas_tweets/lib/funvas_tweets.dart
@@ -8,6 +8,7 @@ export 'package:funvas_tweets/src/7.dart';
 export 'package:funvas_tweets/src/8.dart';
 export 'package:funvas_tweets/src/9.dart';
 export 'package:funvas_tweets/src/10.dart';
+export 'package:funvas_tweets/src/11.dart';
 export 'package:funvas_tweets/src/tweet_mixin.dart';
 
 /// This package contains funvas animations from @creativemaybeno's tweets.
