3c42726289121d489282d7600717f7ccd84e47e3
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-01-14 13:38:25 +0000

    Switch to path URL strategy

--- funvas_demo/lib/main.dart
+++ funvas_demo/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:funvas_demo/widgets/app.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const DemoApp());
 }
