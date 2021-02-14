f73e664e33bd34693973dce4b0abbe70cc4be18b
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-02-14 08:12:21 +0000

    Create fifteen (#61)
    * Prepare merge
    
    * Create fifteen
    
    * Merge references
    
    * Export
    
    * Fix edge case
    
    * Beautify

--- funvas_tweets/lib/funvas_tweets.dart
+++ funvas_tweets/lib/funvas_tweets.dart
@@ -12,6 +12,7 @@ export 'package:funvas_tweets/src/11.dart';
 export 'package:funvas_tweets/src/12.dart';
 export 'package:funvas_tweets/src/13.dart';
 export 'package:funvas_tweets/src/14.dart';
+export 'package:funvas_tweets/src/15.dart';
 export 'package:funvas_tweets/src/tweet_mixin.dart';
 
 /// This package contains funvas animations from @creativemaybeno's tweets.
---
+++ funvas_tweets/lib/src/15.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:funvas/funvas.dart';
+
+class Fifteen extends Funvas {
+  static const _depth = 11;
+  late double _angle;
+
+  @override
+  void u(double t) {
+    c.drawPaint(Paint()..color = const Color(0xfff0d9b5));
+
+    _angle = pi / 2 * _cochleoidX(-pi + 2 * pi * (t % 5) / 5);
+
+    final s = s2q(750), d = s.width;
+    _branch(d / 2, Offset(d / 2, d), 0, _depth);
+  }
+
+  double _cochleoidX(double t) {
+    if (t == 0) return 1;
+    return (sin(t) * cos(t)) / t;
+  }
+
+  void _branch(double d1, Offset p1, double angle, int depth) {
+    if (depth == 0) return;
+    // Half branch distance.
+    final d2 = d1 * 2 / 3;
+    final p2 = Offset(
+      p1.dx + sin(angle) * d2,
+      p1.dy - cos(angle) * d2,
+    );
+    // Stroke based on depth.
+    final paint = Paint()
+      ..color = const Color(0xddb58863)
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = depth / _depth * 6;
+    c.drawLine(p1, p2, paint);
+
+    // Branch left.
+    _branch(d2, p2, angle - _angle, depth - 1);
+    // Branch right.
+    _branch(d2, p2, angle + _angle, depth - 1);
+  }
+}
