6d2aec3a40c633dec322c573361fc789e9e38f75
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-05-13 09:58:45 +0000

    24 (#82)
    * Initial version
    
    * Export 24
    
    * New version
    
    * Improve
    
    * Finish

--- funvas_tweets/export/export_animation.dart
+++ funvas_tweets/export/export_animation.dart
@@ -14,7 +14,7 @@ void main() async {
   // when exporting to GIF.
   const animationName = 'animation';
   // Using a callback so that the constructor is run inside of the test.
-  final funvasFactory = () => TwentyThree();
+  final funvasFactory = () => TwentyFour();
 
   late final ValueNotifier<double> time;
 
---
+++ funvas_tweets/lib/src/24.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:funvas/funvas.dart';
+
+class TwentyFour extends Funvas {
+  static const _l = 1e-2;
+
+  @override
+  void u(double t) {
+    final d = s2q(750).width;
+    c.drawColor(const Color(0xffffffff), BlendMode.srcOver);
+
+    t += 1;
+    t /= 8 / 6;
+    t %= 6;
+
+    c.translate(d / 2, d / 2);
+    c.rotate(-(t / 3 * pi) % (2 * pi));
+    c.translate(-d / 2, -d / 2);
+
+    final r = d / 10.5;
+    drawTrack(t, (d - r * 2 * pi) / 2, d / 2, r, 12, -15);
+  }
+
+  void drawTrack(double t, double x, double y, double r, int n, [int co = 0]) {
+    final bp = Paint()
+      ..color = const Color(0xff000000)
+      ..strokeWidth = 1.5
+      ..style = PaintingStyle.stroke;
+
+    c.drawLine(Offset(x, y), Offset(x + 2 * pi * r, y), bp);
+    drawCircle(
+        Offset(x, y), r, (t * pi) % (12 * pi), n, bp..strokeWidth = 2, co);
+  }
+
+  void drawCircle(
+      Offset center, double radius, double t, int n, Paint paint, int co) {
+    final s = center + Offset(0, -radius);
+
+    Offset cc(t) {
+      // Every 6 pi, we loop around.
+      t %= 6 * pi;
+      if (t < 2 * pi) {
+        return s + Offset(t * radius, 0);
+      } else if (t < 3 * pi) {
+        final cp = s + Offset(2 * pi * radius, 0);
+        return cp +
+            Offset(
+              sin(t) * radius,
+              radius - cos(t) * radius,
+            );
+      } else if (t < 5 * pi) {
+        return s + Offset(2 * pi * radius - (t - 3 * pi) * radius, radius * 2);
+      } else {
+        final cp = s + Offset(0, radius * 2);
+        return cp +
+            Offset(
+              sin(t) * radius,
+              -radius - cos(t) * radius,
+            );
+      }
+    }
+
+    Offset Function(double t) v(double i) {
+      return (double t) {
+        return cc(t) + Offset(cos(t + i) * radius, sin(t + i) * radius);
+      };
+    }
+
+    Color l(double i) =>
+        HSLColor.fromAHSL(1, (i / 2 / pi * 360 + co) % 360, 3 / 4, 1 / 2)
+            .toColor();
+
+    final oc = t - 3 * pi;
+
+    for (var i = 0.0; i < pi * 2; i += pi * 2 / n) {
+      drawPath(v(i), radius, t, oc, l(i));
+    }
+
+    c.drawCircle(cc(t), radius, paint);
+    c.drawCircle(cc(oc), radius, paint);
+    // Make sure the dots are all on top of the lines.
+    for (var i = 0.0; i < pi * 2; i += pi * 2 / n) {
+      c.drawCircle(v(i)(t), 5, Paint()..color = l(i));
+      c.drawCircle(v(i)(oc), 5, Paint()..color = l(i));
+    }
+  }
+
+  void drawPath(Offset Function(double t) o, double radius, double t, double s,
+      Color color) {
+    final start = o(s);
+    final path = Path()..moveTo(start.dx, start.dy);
+    for (var i = s; i < t; i += _l) {
+      final p = o(i);
+      path.lineTo(p.dx, p.dy);
+    }
+    final end = o(t);
+    path.lineTo(end.dx, end.dy);
+    c.drawPath(
+      path,
+      Paint()
+        ..color = color
+        ..blendMode = BlendMode.srcOver
+        ..strokeWidth = 2.5
+        ..style = PaintingStyle.stroke,
+    );
+  }
+}
