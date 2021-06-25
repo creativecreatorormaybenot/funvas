2d8e33f3a967e98dc4576aeae4b982bfa1176651
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-06-25 04:31:53 +0000

    Add 31 (#106)
    * Create animation
    
    * Finish animation
    
    * Export
    
    * Remove tweet

--- funvas_demo/lib/factories/animations.dart
+++ funvas_demo/lib/factories/animations.dart
@@ -28,4 +28,4 @@ final funvasFactories = <int, FunvasFactory<FunvasTweetMixin>>{
 /// tweet yet.
 ///
 /// This one is viewable in debug mode only.
-final wipFunvas = FunvasFactory<Funvas>(() => Thirty());
+final wipFunvas = FunvasFactory<Funvas>(() => ThirtyOne());
