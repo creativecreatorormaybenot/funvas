2deab03442547e1c86e96e85928298d2b0995e26
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-07-10 22:52:24 +0000

    Deploy funvas demo web page

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "de9842486302f31b4749f279174a0735",
-"index.html": "12ee2e9331dccaf326270bb98ca1464d",
-"/": "12ee2e9331dccaf326270bb98ca1464d",
+"main.dart.js": "a69722e80d0646ab1418d8d2fdfe8fa7",
+"index.html": "1e7f231809ba39299c7bd88eebb5ce45",
+"/": "1e7f231809ba39299c7bd88eebb5ce45",
 "manifest.json": "c886259e24bdd821e970fa522119bea0",
 "assets/NOTICES": "13d4946fba46eafc2069f2fd6070b8f5",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
