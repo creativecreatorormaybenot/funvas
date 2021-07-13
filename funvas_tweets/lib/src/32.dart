2671de653b24f618c9f6c030d777e88b76ac76ab
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-07-13 00:19:27 +0000

    Finish

--- funvas_tweets/lib/src/32.dart
+++ funvas_tweets/lib/src/32.dart
@@ -6,47 +6,20 @@ import 'package:funvas/funvas.dart';
 class ThirtyTwo extends Funvas {
   @override
   void u(double t) {
-    final d = s2q(750).width;
+    final h = s2q(750).width / 2;
     c.drawColor(const Color(0xFF303030), BlendMode.srcOver);
-    c.translate(d / 2, d / 2);
-
-    final p = Path();
-    for (var i = 9; i > 0; i--) {
+    c.translate(h, h);
+    int i, j, s;
+    for (i = 9; i > 0; i--) {
       // The piece of code responsible for the angle is inspired by
       // https://www.dwitter.net/d/20577, which was created by pavel.
       // That animation is inspired by Dave Whyte (beesandbombs).
-      var s = 3 << (i ~/ 3) + 1;
-      for (var j = s; j > 0; j--) {
+      for (j = s = 3 << (i ~/ 3) + 1; j > 0; j--) {
         final af = s / (j + i / (1 + pow(4, 4 + i - t % 4.5 * 4)));
-        p.lineTo(
-            Offset.fromDirection(
-              2 * pi / af - pi / 2,
-              i * 37,
-            ).dx,
-            Offset.fromDirection(
-              2 * pi / af - pi / 2,
-              i * 37,
-            ).dy);
-        c.drawCircle(
-          Offset.fromDirection(
-            2 * pi / af - pi / 2,
-            i * 37,
-          ),
-          11 - i / 1.5,
-          Paint()
-            ..color = HSLColor.fromAHSL(
-              1,
-              (-360 / af - 42) % 360,
-              1,
-              3 / 4,
-            ).toColor(),
-        );
+        final p = Offset.fromDirection(2 * pi / af - pi / 2, i * 37);
+        final co = HSLColor.fromAHSL(1, (-360 / af - 42) % 360, 1, 3 / 4);
+        c.drawCircle(p, 11 - i / 1.5, Paint()..color = co.toColor());
       }
     }
-    c.drawPath(
-        p,
-        Paint()
-          ..color = const Color(0xffffffff)
-          ..style = PaintingStyle.stroke);
   }
 }
