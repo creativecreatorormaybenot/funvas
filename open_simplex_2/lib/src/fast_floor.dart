8cc6ebb0eb7087e42146ad318816400bc1acdffc
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-07-17 14:23:49 +0000

    Comment format

--- open_simplex_2/lib/src/fast_floor.dart
+++ open_simplex_2/lib/src/fast_floor.dart
@@ -4,8 +4,8 @@
 /// There is a test case validating that this is indeed (if only slightly)
 /// faster than [floor], always.
 int fastFloor(double x) {
-// We cannot simply return x.toInt() because casting to an integer floors
-// towards zero (truncates) and we want to floor towards negative infinity.
+  // We cannot simply return x.toInt() because casting to an integer floors
+  // towards zero (truncates) and we want to floor towards negative infinity.
   final xi = x.toInt();
   return x < xi ? xi - 1 : xi;
 }
