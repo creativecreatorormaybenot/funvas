89e386aefe91765fcc57d6ea2cd5d79fb7e275d9
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-07-17 03:21:19 +0000

    Finalize example

--- open_simplex_2/example/lib/main.dart
+++ open_simplex_2/example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
+import 'dart:ui';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Image;
 import 'package:funvas/funvas.dart';
 import 'package:open_simplex_2/open_simplex_2.dart';
 
@@ -33,27 +34,33 @@ class ExampleApp extends StatelessWidget {
 /// website (https://bleuje.github.io/tutorial3/).
 /// You can find it here: https://gist.githubusercontent.com/Bleuje/0ee88547c273b6ae49ae69527c13e611/raw/a28fb770ab6586a11cda227c44af0ac57f45e8d7/tuto3_entirecode.pde.
 class _OpenSimplex2Funvas extends Funvas {
+  static const dimension = 100.0;
+
   final noise = OpenSimplex2F(12345);
 
   @override
   void u(double t) {
-    s2q(100);
+    s2q(dimension);
     c.drawColor(const Color(0xff000000), BlendMode.srcOver);
+
+    t /= 2;
 
     const m = 90;
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < m; j++) {
-        final x = 100 * i / (m - 1);
-        final y = 100 * j / (m - 1);
+        const margin = dimension / 10;
+        final x = (dimension - margin * 2) * i / (m - 1) + margin;
+        final y = (dimension - margin * 2) * j / (m - 1) + margin;
 
         final dx = 20.0 * periodicFunction(t - offset(x, y), 0, x, y);
         final dy = 20.0 * periodicFunction(t - offset(x, y), 123, x, y);
 
         c.drawCircle(
           Offset(x + dx, y + dy),
-          0.5,
+          dimension / 200,
           Paint()
-            ..color = const Color.fromARGB(50, 255, 255, 255),
+            ..color = const Color.fromARGB(50, 255, 255, 255)
+            ..blendMode = BlendMode.plus,
         );
       }
     }
@@ -71,6 +78,6 @@ class _OpenSimplex2Funvas extends Funvas {
   }
 
   double offset(double x, double y) {
-    return 0.015 * sqrt(pow(50 - x, 2) + pow(50 - y, 2));
+    return 0.015 * sqrt(pow(dimension / 2 - x, 2) + pow(dimension / 2 - y, 2));
   }
 }
