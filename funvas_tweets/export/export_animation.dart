258e42e48c96d21844bbf71d1566b3153fad293b
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-07-19 20:31:36 +0000

    Create 34

--- funvas_tweets/export/export_animation.dart
+++ funvas_tweets/export/export_animation.dart
@@ -9,13 +9,13 @@ import 'package:funvas_tweets/funvas_tweets.dart';
 
 void main() {
   const fps = 50;
-  const animationDuration = Duration(seconds: 11);
+  const animationDuration = Duration(seconds: 7);
   const dimensions = Size(750, 750);
   // If you use a different animation name, you will have to also consider that
   // when exporting to GIF.
   const animationName = 'animation';
   // Using a callback so that the constructor is run inside of the test.
-  Funvas funvasFactory() => ThirtyThree();
+  Funvas funvasFactory() => ThirtyFour();
 
   late final ValueNotifier<double> time;
 
---
+++ funvas_tweets/lib/src/34.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:funvas/funvas.dart';
+import 'package:open_simplex_2/open_simplex_2.dart';
+
+class ThirtyFour extends Funvas {
+  /// Base precision for the paths being drawn.
+  ///
+  /// A value of `2` will result in twice as many path segments per circle
+  /// as `1`. Smaller circles inherently use fewer segments than larger circles.
+  static const _precision = 1.0;
+
+  /// Disturbance for the roundness of the circles drawn (= how much the noise
+  /// distorts the circle paths).
+  static const _disturbance = 424.24;
+
+  /// How much the circles should be distorted over time.
+  ///
+  /// This is similar to [_disturbance] but only affects the relative speed of
+  /// the distortions, where bigger is faster.
+  static const _wobbliness = 1 / 2;
+
+  /// How many times a circle should reappear (from the center) within the
+  /// [_period].
+  ///
+  /// The larger the faster the circles grow.
+  static const _revolutions = 2;
+
+  static const _dimension = 750.0, _period = 7, _n = 20;
+
+  final _noise = OpenSimplex2S(42);
+
+  @override
+  void u(double t) {
+    c.drawColor(const Color(0xff000000), BlendMode.srcOver);
+    final s = s2q(_dimension);
+    t %= _period;
+    c.translate(s.width / 2, s.height / 2);
+    for (var i = 0; i < _n; i++) {
+      _drawStep(i, t);
+    }
+  }
+
+  void _drawStep(int i, double t) {
+    const di = sqrt2 * _dimension / 1.5;
+    const dr = di / _n;
+    const tsr = _period / (_revolutions * _n);
+    final ts = ((t - tsr * i) % (_period / _revolutions)) / tsr * dr;
+    final ta = t * 2 * pi / _period;
+    final so = Offset(i * 20, i * 20);
+    _drawCircle(ta, ts, so);
+  }
+
+  void _drawCircle(double ta, double r, Offset so) {
+    final precision = 2 * pi / r / _precision;
+    final tx = sin(ta) * _wobbliness, ty = cos(ta) * _wobbliness;
+
+    final path = Path();
+    for (var a = 0.0; a < 2 * pi; a += precision) {
+      final dx = sin(a), dy = cos(a);
+      final nr = r +
+          _noise.noise4XYBeforeZW(dx + so.dx, dy + so.dy, tx, ty) *
+              _disturbance *
+              r /
+              sqrt2 /
+              _dimension;
+      final x = dx * nr, y = dy * nr;
+      if (a == 0) {
+        path.moveTo(x, y);
+      } else {
+        path.lineTo(x, y);
+      }
+    }
+    c.drawPath(
+      path..close(),
+      Paint()
+        ..color = const Color(0xafffffff)
+        ..strokeWidth = 4.2
+        ..style = PaintingStyle.stroke,
+    );
+  }
+}
