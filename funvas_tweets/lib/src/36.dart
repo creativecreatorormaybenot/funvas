9bffaf38d8bcd7b17ce7a39214c39f3f6adb3d5c
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-08-01 17:46:16 +0000

    Order

--- funvas_tweets/lib/src/36.dart
+++ funvas_tweets/lib/src/36.dart
@@ -20,19 +20,19 @@ class ThirtySix extends Funvas {
     c.translate(0, d);
     c.scale(1, -1);
 
-    // n has to be a power of 2. 128 is the last smooth one.
-    const n = 16, sw = 256 / n, s = (d - sw) / (n - 1);
+    // Minimum order is first order. 7 is the last smooth (real time) order.
+    const order = 4;
+    final n = pow(2, order) ~/ 1, sw = 256 / n, s = (d - sw) / (n - 1);
     final p = Path()..moveTo(sw / 2, sw / 2);
 
     // D is the duration of the animation in seconds.
-    const D = 18, l = n * n;
+    const D = 18;
     final r = t / D % 1;
-    final lp = l * r;
-    Offset transform(Point<double> c) =>
-        Offset(c.x * s + sw / 2, c.y * s + sw / 2);
+    final l = n * n, lp = l * r;
+    Offset transform(_Crd c) => Offset(c.x * s + sw / 2, c.y * s + sw / 2);
     void line(Offset o) => p.lineTo(o.dx, o.dy);
 
-    // Draw the Hilbert curve (order n-1) up to a certain node based on time.
+    // Draw the Hilbert curve up to a certain node based on time.
     for (var i = 0; i < lp; i++) {
       line(transform(_hni2cc(i, n)));
     }
@@ -59,9 +59,9 @@ class ThirtySix extends Funvas {
 }
 
 /// Transforms the given node index [i] on a pseudo Hilbert curve of order
-/// `n -1` to a Cartesian coordinate (Hilbert node index to Cartesian
+/// `2^order = n` to a Cartesian coordinate (Hilbert node index to Cartesian
 /// coordinate).
-Point<double> _hni2cc(int i, int n) {
+_Crd _hni2cc(int i, int n) {
   // The fixed positions of the first order Hilbert curve (n=2).
   const n2positions = <Point<int>>[
     Point(0, 0),
@@ -100,10 +100,12 @@ Point<double> _hni2cc(int i, int n) {
 
     j >>>= 2;
   }
-  return Point<double>(x.toDouble(), y.toDouble());
+  return _Crd(x.toDouble(), y.toDouble());
 }
 
 /// Returns the last two bits of the given integer [x].
 ///
 /// With can simply do this using bitwise AND and `b011` (which is decimal `3`).
 int _l2b(int x) => x & /*int.parse('11', radix: 2)*/ 3;
+
+typedef _Crd = Point<double>;
