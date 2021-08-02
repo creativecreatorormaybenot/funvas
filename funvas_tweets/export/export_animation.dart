b8dab2791d7fc968fb54c9c259b67481360324d9
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-08-02 14:31:35 +0000

    Export

--- funvas_tweets/export/export_animation.dart
+++ funvas_tweets/export/export_animation.dart
@@ -15,7 +15,7 @@ void main() {
   // when exporting to GIF.
   const animationName = 'animation';
   // Using a callback so that the constructor is run inside of the test.
-  Funvas funvasFactory() => ThirtySix();
+  Funvas funvasFactory() => ThirtySeven();
 
   late final ValueNotifier<double> time;
 
