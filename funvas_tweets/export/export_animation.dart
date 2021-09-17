4112369b799cd9d4eb19b1a4c8bb4430f7601ed3
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-09-17 19:14:10 +0000

    Use correct animation

--- funvas_tweets/export/export_animation.dart
+++ funvas_tweets/export/export_animation.dart
@@ -15,7 +15,7 @@ void main() {
   // when exporting to GIF.
   const animationName = 'animation';
   // Using a callback so that the constructor is run inside of the test.
-  Funvas funvasFactory() => ThirtyEight();
+  Funvas funvasFactory() => ThirtyNine();
 
   late final ValueNotifier<double> time;
 
