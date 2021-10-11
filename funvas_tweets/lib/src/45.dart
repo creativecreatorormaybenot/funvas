8babeb47303fd10080c827fda520cd20201bb5ba
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-11 18:05:37 +0000

    Get alignment somewhat right

--- funvas_tweets/lib/src/45.dart
+++ funvas_tweets/lib/src/45.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/rendering.dart';
@@ -30,28 +29,37 @@ class FortyFive extends Funvas {
 
     const d = 750.0;
     s2q(d);
+    const scale = d / _h, smallerScale = scale / 142;
+    const scaledH = _h * scale, scaledW = _w * scale;
 
-    c.translate(d / 2, d / 4);
-    c.scale(1 + t % 6);
-    c.translate(-d / 2, d / -4);
+    const relativeX = -0.0954, relativeY = 0.2345;
+    const relativeWidthGap = (_h - _w) / _h / 2;
 
-    final scale = d / max(_w, _h);
+    const dx = relativeWidthGap * d +
+            scaledW / 2 +
+            scaledW * relativeX +
+            _w * smallerScale * relativeX * 0.25,
+        dy = scaledH * relativeY - _h * smallerScale * relativeY * 2.25;
+    c.translate(dx, dy);
+    c.scale(1);
+    c.translate(-dx, -dy);
+
     final monaLisas = <RSTransform>[
       RSTransform.fromComponents(
         rotation: 0,
         scale: scale,
-        anchorX: 0,
+        anchorX: _w / 2,
         anchorY: 0,
-        translateX: _w * scale / 4,
+        translateX: d / 2,
         translateY: 0,
       ),
       RSTransform.fromComponents(
         rotation: 0,
-        scale: scale / 142,
-        anchorX: _w / 2,
-        anchorY: _h / 2,
-        translateX: _w * scale / 4 + _w * scale / 2.468,
-        translateY: _h * scale / 4.297,
+        scale: smallerScale,
+        anchorX: _w / 2 + _w * relativeX,
+        anchorY: _h / 2 + _h * relativeY,
+        translateX: d / 2 + scaledW * relativeX,
+        translateY: scaledH * relativeY,
       ),
     ];
 
@@ -67,7 +75,6 @@ class FortyFive extends Funvas {
       null,
       Paint(),
     );
-    c.drawImage(monaLisa, const Offset(_w / 4, 0), Paint());
   }
 
   Future<void> _loadImage() async {
