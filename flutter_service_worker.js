72795023d42f4bde625a6c2d5b52566b6150f535
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-13 23:44:48 +0000

    Deploy funvas gallery app

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "main.dart.js": "6dcde3aa42e15bf0f667a593925fbf88",
 "manifest.json": "7b5ec2284650bed4f12efdd78d647ff3",
-"index.html": "da36e878bed1968331a18b359b14e62c",
-"/": "da36e878bed1968331a18b359b14e62c"
+"index.html": "409c68efb4bf81e750295e5e70195a3d",
+"/": "409c68efb4bf81e750295e5e70195a3d"
 };
 
 // The application shell files that are downloaded before a service worker can
