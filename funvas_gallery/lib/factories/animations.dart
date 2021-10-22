22e147724803b9ea3abcb3e9ccc45e8c5a7d4b4e
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2021-10-22 17:42:49 +0000

    Include 47 in the gallery (#179)

--- funvas_gallery/lib/factories/animations.dart
+++ funvas_gallery/lib/factories/animations.dart
@@ -30,6 +30,7 @@ final funvasFactories = <int, FunvasFactory<FunvasTweetMixin>>{
   40: FunvasFactory(() => Forty()),
   41: FunvasFactory(() => FortyOne()),
   44: FunvasFactory(() => FortyFour()),
+  47: FunvasFactory(() => FortySeven()),
 };
 
 /// Funvas factory for a WIP funvas that does not need to have an associated
