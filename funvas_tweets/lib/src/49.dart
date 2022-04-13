944fa5f8b575d17afa86a78d950b7140c1e2f2d1
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-04-13 06:15:38 +0000

    Refine

--- funvas_tweets/lib/src/49.dart
+++ funvas_tweets/lib/src/49.dart
@@ -7,32 +7,37 @@ import 'package:funvas/funvas.dart';
 class FortyNine extends Funvas {
   @override
   void u(double t) {
+    const period = 6;
+    const factor = 1.75;
+    const n = 5e3;
+
+    t %= period;
+
     const d = 750.0;
     s2q(d);
     c.drawColor(const Color(0xff000000), BlendMode.srcOver);
 
     c.translate(d / 2, d / 2);
-    c.rotate(pi * t / 9 * 2);
+    c.scale(1 / 2);
+    c.rotate(-pi / 2);
 
-    const n = 3e3;
     Offset oo(int i) => Offset.fromDirection(
-          S(i + t / 9).abs() * pi * 2,
+          S(i * pi / factor + t / (period * factor * 4) * pi * 2) * pi * 2,
           d / n * (n - i) / 1.25,
         );
 
-    c.rotate(-t / 9 * pi * 2 - pi / 2);
     for (var i = 0; i < n; i++) {
       final o = oo(i), no = oo(i + 1);
       final p = Path()
-        ..moveTo(o.dx, o.dy)
-        ..lineTo(no.dx, no.dy);
+        ..moveTo(no.dx, no.dy)
+        ..lineTo(o.dx, o.dy);
 
       final paint = Paint()
         ..style = PaintingStyle.stroke
         ..blendMode = BlendMode.plus
-        ..color = HSVColor.fromAHSV(1, 360 / n * i, 3 / 4, 3 / 4).toColor();
+        ..strokeWidth = 3
+        ..color = HSVColor.fromAHSV(1 / 4, 360 / n * i, 3 / 4, 3 / 4).toColor();
 
-      if (p.computeMetrics().first.length > 420) continue;
       c.drawPath(p, paint);
     }
   }
