d4355ffb6d4dbcc94207a33274f4359020d898be
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-07 14:32:25 +0000

    Fix mod

--- funvas_tweets/shaders/glsl/50.glsl
+++ funvas_tweets/shaders/glsl/50.glsl
@@ -6,26 +6,26 @@ layout(location = 0) uniform vec2 iResolution;
 layout(location = 1) uniform float t;
 
 void main() {
-  int iterations = 100;
-  int rotationDuration = 9;
-  int threshold = 16;
+  float iterations = 100;
+  float rotationDuration = 9;
+  float threshold = 16;
 
   float pi = 3.14159265359;
 
-  float zoom = cos(t % rotationDuration / rotationDuration * 2 * pi) * .5;
+  float zoom = cos(mod(t, rotationDuration) / rotationDuration * 2 * pi) * .5;
   vec2 reRange = vec2(-1.65 + .65 - zoom, 1.65 - .65 + zoom);
   vec2 imRange = vec2(-1.65 + .65 - zoom, 1.65 - .65 + zoom);
 
   vec2 centerPoint = vec2(-0.75, 0);
   vec2 c = vec2(
-    centerPoint.x + cos(t % rotationDuration / rotationDuration * 2 * pi) * .05,
-    centerPoint.y + sin(t % rotationDuration / rotationDuration * 2 * pi) * .1
+    centerPoint.x + cos(mod(t, rotationDuration) / rotationDuration * 2 * pi) * .05,
+    centerPoint.y + sin(mod(t, rotationDuration) / rotationDuration * 2 * pi) * .1
   );
 
   float re = (reRange.y - reRange.x) * (gl_FragCoord.x / iResolution.x);
   float im = (imRange.y - imRange.x) * (gl_FragCoord.y / iResolution.y);
 
-  int n = 0;
+  float n = 0;
   float zre = re, zim = im;
   while (n < iterations) {
     float tzre = zre * zre - zim * zim + c.x;
