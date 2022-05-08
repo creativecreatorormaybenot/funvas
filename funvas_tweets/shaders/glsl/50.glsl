2eaa02373774a2c32f2a8edd74bdc435722c9488
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-08 10:45:01 +0000

    Limit zoom

--- funvas_tweets/shaders/glsl/50.glsl
+++ funvas_tweets/shaders/glsl/50.glsl
@@ -19,7 +19,7 @@ vec3 hsl2rgb(in vec3 c) {
 }
 
 void main() {
-  float scale = pow(2, t) - 1;
+  float scale = pow(2, mod(t, 14)) - 0.5;
 
   vec2 rel = gl_FragCoord.xy / res.xy;
   rel -= vec2(0.5);
