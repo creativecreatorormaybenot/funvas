164d659519e8b14af0be284fd3bbe6487a42ee0d
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-08 18:05:41 +0000

    Revert to reasonable export values

--- funvas_rendering/lib/main.dart
+++ funvas_rendering/lib/main.dart
@@ -12,9 +12,9 @@ import 'package:funvas/funvas.dart';
 import 'package:funvas_tweets/funvas_tweets.dart';
 import 'package:path/path.dart' as p;
 
-const fps = 60;
+const fps = 50;
 const animationDuration = Duration(seconds: 15);
-const dimensions = Size(1500, 1500);
+const dimensions = Size(750, 750);
 // If you use a different animation name, you will have to also consider that
 // when assembling the animation using ffmpeg.
 const animationName = 'animation';
