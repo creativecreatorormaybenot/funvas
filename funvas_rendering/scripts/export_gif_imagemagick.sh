0c532c1befe434996aeef857dfabd8db56170ee2
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-08 15:21:42 +0000

    Remove data

--- funvas_rendering/scripts/export_gif_imagemagick.sh
+++ funvas_rendering/scripts/export_gif_imagemagick.sh
@@ -1,5 +1,6 @@
 rm -rf export/animation/
 mkdir -p export/animation/
+rm -rf $HOME/Library/Containers/creativemaybeno.funvasRendering/Data/
 flutter run
 mv $HOME/Library/Containers/creativemaybeno.funvasRendering/Data/export/animation/* ./export/animation/
 convert -delay 2 export/animation/*.png -loop 0 export/animation.gif
--- funvas_rendering/scripts/export_mp4.sh
+++ funvas_rendering/scripts/export_mp4.sh
@@ -1,5 +1,6 @@
 rm -rf export/animation/
 mkdir -p export/animation/
+rm -rf $HOME/Library/Containers/creativemaybeno.funvasRendering/Data/
 flutter run
 mv $HOME/Library/Containers/creativemaybeno.funvasRendering/Data/export/animation/* ./export/animation/
 ffmpeg -r 50 -f image2 -s 750x750 -i export/animation/%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p export/animation.mp4
