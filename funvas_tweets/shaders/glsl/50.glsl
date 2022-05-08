d3ab093ddaad5fbbfd0025cd1b46c40a82224eeb
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-08 10:28:21 +0000

    Zoom onto spirals

--- funvas_tweets/shaders/glsl/50.glsl
+++ funvas_tweets/shaders/glsl/50.glsl
@@ -5,10 +5,10 @@ layout(location = 0) out vec4 fragColor;
 layout(location = 0) uniform vec2 res;
 layout(location = 1) uniform float t;
 
-const float iterations = 100;
+const float iterations = 700;
 const float threshold = 16;
 
-const vec2 zoomPoint = vec2(-1.74999841099374081749002483162428393452822172335808534616943930976364725846655540417646727085571962736578151132907961927190726789896685696750162524460775546580822744596887978637416593715319388030232414667046419863755743802804780843375, -0.00000000000000165712469295418692325810961981279189026504290127375760405334498110850956047368308707050735960323397389547038231194872482690340369921750514146922400928554011996123112902000856666847088788158433995358406779259404221904755);
+const vec2 zoomPoint = vec2(-0.761574,-0.0847596);
 const vec2 reRange = vec2(-2, 1);
 const vec2 imRange = vec2(-1.5, 1.5);
 
@@ -19,7 +19,7 @@ vec3 hsl2rgb(in vec3 c) {
 }
 
 void main() {
-  float scale = t + 0.5;
+  float scale = pow(2, t) + 0.25;
 
   vec2 rel = gl_FragCoord.xy / res.xy;
   rel -= vec2(0.5);
@@ -29,8 +29,8 @@ void main() {
   vec2 center = vec2(reRange.y + reRange.x, imRange.y + imRange.x) / 2.0;
   vec2 offset = zoomPoint - center;
 
-  float re = reRange.x + (reRange.y - reRange.x) * rel.x - offset.x;
-  float im = imRange.x + (imRange.y - imRange.x) * rel.y - offset.y;
+  float re = reRange.x + (reRange.y - reRange.x) * rel.x + offset.x;
+  float im = imRange.x + (imRange.y - imRange.x) * rel.y + offset.y;
 
   float n = 0;
   float zre = re, zim = im;
@@ -40,14 +40,15 @@ void main() {
     zre = tzre;
 
     n = tn;
-    if (abs(zre) + abs(zim) > threshold)
+    if (abs(zre) + abs(zim) > threshold) {
       break;
+    }
   }
 
   if (n == iterations) {
     fragColor = vec4(0, 0, 0, 1);
   } else {
-    vec3 hsl = vec3(n / iterations, .9, .6);
+    vec3 hsl = vec3(n / 100, .9, .6);
     vec3 rgb = hsl2rgb(hsl);
     fragColor = vec4(rgb, 1);
   }
