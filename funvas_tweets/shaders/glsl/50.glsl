6a4d63d9c68ff7d9b8c94663289f87ba2076cc0b
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2022-05-08 10:17:15 +0000

    Implement zoom

--- funvas_tweets/shaders/glsl/50.glsl
+++ funvas_tweets/shaders/glsl/50.glsl
@@ -5,7 +5,7 @@ layout(location = 0) out vec4 fragColor;
 layout(location = 0) uniform vec2 res;
 layout(location = 1) uniform float t;
 
-const float iterations = 200;
+const float iterations = 100;
 const float threshold = 16;
 
 const vec2 zoomPoint = vec2(-1.74999841099374081749002483162428393452822172335808534616943930976364725846655540417646727085571962736578151132907961927190726789896685696750162524460775546580822744596887978637416593715319388030232414667046419863755743802804780843375, -0.00000000000000165712469295418692325810961981279189026504290127375760405334498110850956047368308707050735960323397389547038231194872482690340369921750514146922400928554011996123112902000856666847088788158433995358406779259404221904755);
@@ -19,20 +19,18 @@ vec3 hsl2rgb(in vec3 c) {
 }
 
 void main() {
+  float scale = t + 0.5;
+
   vec2 rel = gl_FragCoord.xy / res.xy;
+  rel -= vec2(0.5);
+  rel /= scale;
+  rel += vec2(0.5);
 
-  float width = reRange.y - reRange.x;
-  float height = imRange.y - imRange.x;
-//   float zoomedWidth = width / (0.5 + mod(t, 5));
-//   float zoomedHeight = height / (0.5 + mod(t, 5));
-  float zoomedWidth = width, zoomedHeight = height;
-  float rl = zoomPoint.x - zoomedWidth / 2;
-  float rt = zoomPoint.y - zoomedHeight / 2;
-  float rr = zoomPoint.x + zoomedWidth / 2;
-  float rb = zoomPoint.y + zoomedHeight / 2;
+  vec2 center = vec2(reRange.y + reRange.x, imRange.y + imRange.x) / 2.0;
+  vec2 offset = zoomPoint - center;
 
-  float re = reRange.x + (reRange.y - reRange.x) * rel.x;
-  float im = imRange.x + (imRange.y - imRange.x) * rel.y;
+  float re = reRange.x + (reRange.y - reRange.x) * rel.x - offset.x;
+  float im = imRange.x + (imRange.y - imRange.x) * rel.y - offset.y;
 
   float n = 0;
   float zre = re, zim = im;
